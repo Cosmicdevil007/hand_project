90f5a109cf0dc8125b3dbd2fe0e141214bbc97e2
Cosmicdevil007 <ada002791@gmail.com>
2025-05-19 15:06:46 +0530

    Update files

--- web_app.py
+++ web_app.py
@@ -202,6 +202,6 @@ def live_video():
     #    cap = None
     #return jsonify({'status': 'success'})
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app_flask.run(host='0.0.0.0', port=port, debug=True)
+#if __name__ == '__main__':
+ #   port = int(os.environ.get("PORT", 8080))
+    #app_flask.run(host='0.0.0.0', port=port, debug=True)
